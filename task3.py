2abcd7e8694446b5596bbefcd1f563cc292df5af
Denys Burlakov <denys.burlakov@Netcracker.com>
2024-07-28 22:04:12 +0300

    Added implementation for HW 07.

---
+++ task3.py
@@ -0,0 +1,12 @@
+from avltree import *
+# Driver program to test the above functions
+root = None
+keys = [10, 20, 30, 25, 28, 27, -1]
+
+for key in keys:
+    root = insert(root, key)
+    print("Вставлено:", key)
+    print("AVL-Дерево:")
+    print(root)
+
+print(f"Total sum of all values: {sum_values(root)}")
